274ad16602af7e3a8c6e1f18781a1326d393d7b4
Yuuiti Nakamura <s20c1081bt@s.chibakoudai.jp>
2022-01-07 12:02:40 +0000

    a

--- scripts/count.py
+++ scripts/count.py
@@ -1,4 +1,4 @@
-#1/usr/bin/env python3
+#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int32
 
@@ -8,5 +8,5 @@ rate = rospy.Rate(10)                                   # 10Hzで実行
 n = 0
 while not rospy.is_shutdown():
         n += 1
-            pub.publish(n)
-                rate.sleep()
+        pub.publish(n)
+        rate.sleep()
